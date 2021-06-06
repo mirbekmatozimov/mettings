e90ee0858ee596c74ce01a5f4af979e6c96f24fe
mirbek <mirbek.25.52@gmail.com>
2021-06-06 15:12:14 +0600

    Updates

---
+++ precache-manifest.a47726594a30b6f4e6e6aced6886dee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a67835e71f8e7312dd96721c450086",
+    "url": "/mettings/index.html"
+  },
+  {
+    "revision": "3d80dd08f38436834e62",
+    "url": "/mettings/static/css/main.f1524157.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/mettings/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mettings/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d80dd08f38436834e62",
+    "url": "/mettings/static/js/main.8bba3323.chunk.js"
+  },
+  {
+    "revision": "7b721f0def9c9aaabb8a",
+    "url": "/mettings/static/js/runtime-main.5b02c6a6.js"
+  }
+]);
